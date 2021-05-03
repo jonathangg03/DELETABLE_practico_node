4419ce461e3d249b2e8b325b1ad605cb76a3dea9
Jonathan García <jona03g97@gmail.com>
2021-05-03 16:16:41 -0600

    Autenticación: gestión de permisos

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,48 @@
 const jwt = require("jsonwebtoken");
+const config = require("../config");
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, "secreto");
+  return jwt.sign(data, secret);
 }
+
+function verify(token) {
+  return jwt.verify(token, secret);
+}
+
+function getToken(authorization) {
+  //Tokens de authorizacion se ven como: Bearer esf8swh83hw8eh
+  if (!authorization) {
+    throw new Error("No viene token");
+  }
+
+  if (authorization.indexOf("Bearer ") === -1) {
+    //Sí no encuentra la palabra Bearer
+    throw new Error("Token invalido");
+  }
+
+  let token = authorization.replace("Bearer", ""); //Para quitar el Bearer y solo quedarnos con el token
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || "";
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+
+  return decoded;
+}
+
+const check = {
+  own: function (req, owner) {
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+  },
+};
 
 module.exports = {
   sign,
+  check,
 };

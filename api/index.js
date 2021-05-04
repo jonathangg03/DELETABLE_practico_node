e36b94b1daca873cf2e4a876fbbd4168c4d4a376
Jonathan García <jona03g97@gmail.com>
2021-05-03 19:23:17 -0600

    Gestión avanzada de errores: Throw

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const user = require("./components/user/network");
 const auth = require("./components/auth/network");
 const swaggerUI = require("swagger-ui-express");
 const swaggerDoc = require("./swagger.json");
+const errors = require("../network/errors");
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", user);
 app.use("/api/auth", auth);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use(errors); //DEBE SER EL ULTIMO
 
 app.listen(config.api.port, () => {
   console.log(`API escuchando en el puerto ${config.api.port}`);
--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config");
+const error = require("../utils/error");
 const secret = config.jwt.secret;
 
 function sign(data) {
@@ -45,7 +46,7 @@ const check = {
     console.log(decoded);
 
     if (decoded.id !== owner) {
-      throw new Error("No tienes autorización de hacer eso");
+      throw error("No puedes hacer esto", 401);
     }
   },
 };
